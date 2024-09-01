ba98f3b4d20cd321ec9258d49e6f196064a998ab
rakesh <rakeshrobin32153@gmail.com>
2024-09-01 21:23:51 +0530

    remove duplicate from string

---
+++ CommonInterviewQuestion/RemoveDuplicate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    /*
+     * Remove Duplicate From String
+     */
+    internal class RemoveDuplicate
+    {
+        public static void Main(string[] args)
+        {
+            string inputString = "HHeelloo WWoorrlldd";
+            Console.WriteLine(RemoveDuplicateFromString(inputString));
+        }
+
+        public static string RemoveDuplicateFromString(string inputString)
+        {
+            string result = string.Empty;
+            foreach (char c in inputString)
+            {
+                if (!result.Contains(c))
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+    }
+}
