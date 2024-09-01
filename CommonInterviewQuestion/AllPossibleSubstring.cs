9380e37ab1bb5acb802f53df25c24be05411d37a
rakesh <rakeshrobin32153@gmail.com>
2024-09-01 22:01:53 +0530

    commit

---
+++ CommonInterviewQuestion/AllPossibleSubstring.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    /*
+     * Find all possible substring of a given string
+     */
+    internal class AllPossibleSubstring
+    {
+        public static void Main(string[] args)
+        {
+            string inputString = "abcd";
+
+            GenerateSubstring(inputString);
+        }
+
+        public static void GenerateSubstring(string inputString)
+        {           
+            for (int i = 0; i <= inputString.Length; i++)
+            {
+                StringBuilder substring = new StringBuilder(inputString.Length - i);
+                for (int j = i; j < inputString.Length; j++)
+                {
+                    substring.Append(inputString[j]);
+                    Console.Write(substring + " ");
+                }
+            }
+        }
+    }
+}
