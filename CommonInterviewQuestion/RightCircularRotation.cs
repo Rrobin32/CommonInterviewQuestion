112f3d128e1b96a81f2f8bafeb5da9f8dc8832b3
rakesh <rakeshrobin32153@gmail.com>
2024-09-02 21:58:54 +0530

    /*   * Right circular rotation of an array   * 1 2 3 4 5 => 5 1 2 3 4   */

---
+++ CommonInterviewQuestion/RightCircularRotation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    /*
+    * Right circular rotation of an array
+    * 1 2 3 4 5 => 5 1 2 3 4
+    */
+    internal class RightCircularRotation
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            TraverseArray(array);
+        }
+
+        public static void TraverseArray(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                temp = array[0];
+                array[0] = array[i + 1];
+                array[i + 1] = temp;
+            }
+
+            foreach (int num in array)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
