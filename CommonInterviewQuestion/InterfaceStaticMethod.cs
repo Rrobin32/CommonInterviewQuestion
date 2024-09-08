a6b3b467b4f5ffa8d16c565310138202a148af82
rakesh <rakeshrobin32153@gmail.com>
2024-09-08 14:28:10 +0530

    static method interface

---
+++ CommonInterviewQuestion/InterfaceStaticMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    public interface IInterfaceStaticMethod
+    {
+        public static void InterfaceStaticMethod()
+        {
+            Console.WriteLine("Interface Static Method");
+        }
+    }
+
+    internal class InterfaceStaticMethod
+    {
+        public static void Main(string[] args)
+        {
+            IInterfaceStaticMethod.InterfaceStaticMethod();
+        }
+    }
+}
