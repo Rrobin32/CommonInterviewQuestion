81c175b7e8553f9b2932117cccb4fdf004553448
rakesh <rakeshrobin32153@gmail.com>
2024-09-09 22:14:43 +0530

    Reference Type VS Value Type

---
+++ CommonInterviewQuestion/ValueVsReferenceType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    internal class ValueVsReferenceType
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = a;
+            b = 20;
+
+            List<int> ints = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> ints1 = ints;
+            ints1.Add(6);
+
+            Console.WriteLine("a : " + a + ", b: " + b);
+            Console.WriteLine("ints count : " + ints.Count + ", ints1 count : " + ints1.Count);
+        }
+    }
+}
