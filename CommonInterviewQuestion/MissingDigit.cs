fa9fbed361f59649ef82237d30b893b003b3806e
rakesh <rakeshrobin32153@gmail.com>
2024-09-13 23:15:47 +0530

    commit

---
+++ CommonInterviewQuestion/MissingDigit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    internal class MissingDigit
+    {
+        public static void Main(string[] args)
+        {
+            string inputExpression = "x + 4 = 6";
+            string missingDigit = FindMissingDigit(inputExpression);
+            Console.WriteLine(missingDigit);
+        }
+
+        public static string FindMissingDigit(string expression)
+        {
+            return string.Empty;
+        }
+    }
+
+    
+}
