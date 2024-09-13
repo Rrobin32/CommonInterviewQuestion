c084817b2b8acd2b6f0385af8dff2aec962bb814
rakesh <rakeshrobin32153@gmail.com>
2024-09-13 23:39:05 +0530

    Missing Digit

--- CommonInterviewQuestion/MissingDigit.cs
+++ CommonInterviewQuestion/MissingDigit.cs
@@ -10,16 +10,100 @@ namespace CommonInterviewQuestion
     {
         public static void Main(string[] args)
         {
-            string inputExpression = "x + 4 = 6";
-            string missingDigit = FindMissingDigit(inputExpression);
+            string inputExpression = "100 * x = 500";
+            int missingDigit = FindMissingDigit(inputExpression);
             Console.WriteLine(missingDigit);
         }
 
-        public static string FindMissingDigit(string expression)
+        public static int FindMissingDigit(string expression)
         {
-            return string.Empty;
+            string[] expArr = expression.Split(' ');
+            string firstOperand = expArr[0];
+            string secondOperand = expArr[2];
+            string @operator = expArr[1];
+            string result = expArr[4];
+
+            int res = 0;
+            if (firstOperand.Equals("x"))
+            {
+                int second = int.Parse(secondOperand);
+                int _result = int.Parse(result);
+
+                switch (@operator)
+                {
+                    case ("+"):
+                        res = _result - second;
+                        break;
+
+                    case ("-"):
+                        res = _result + second;
+                        break;
+
+                    case ("*"):
+                        res = _result / second;
+                        break;
+
+                    default:
+                        res = _result * second;
+                        break;
+
+                }
+            }
+
+            else if (secondOperand.Equals("x"))
+            {
+                int first = int.Parse(firstOperand);
+                int _result = int.Parse(result);
+
+                switch (@operator)
+                {
+                    case ("+"):
+                        res = _result - first;
+                        break;
+
+                    case ("-"):
+                        res = first - _result;
+                        break;
+
+                    case ("*"):
+                        res =  _result / first;
+                        break;
+
+                    default:
+                        res = first / _result;
+                        break;
+
+                }
+            }
+            else
+            {
+                int first = int.Parse(firstOperand);
+                int second = int.Parse(secondOperand);
+
+                switch (@operator)
+                {
+                    case ("+"):
+                        res = first + second;
+                        break;
+
+                    case ("-"):
+                        res = first - second;
+                        break;
+
+                    case ("*"):
+                        res = first * second;
+                        break;
+
+                    default:
+                        res = first / second;
+                        break;
+
+                }
+            }
+
+            return res;
         }
     }
 
-    
+
 }
