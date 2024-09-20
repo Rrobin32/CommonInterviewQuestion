8141739e32ad4c8ba643416c0492a35d7b40353d
rakesh <rakeshrobin32153@gmail.com>
2024-09-20 22:10:40 +0530

    Singleton pattern

---
+++ CommonInterviewQuestion/SingletonImplementation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    internal sealed class SingletonImplementation
+    {
+        public SingletonImplementation() { }
+
+        private static SingletonImplementation instance;
+        public static SingletonImplementation singleton
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonImplementation();
+                }
+                return instance;
+            }
+        }
+    }
+
+    internal class Singleton
+    {
+        public static void Main(string[] args)
+        {
+            SingletonImplementation s1 = SingletonImplementation.singleton;
+            SingletonImplementation s2 = SingletonImplementation.singleton;
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Same Instance");
+            }
+        }
+    }
+}
