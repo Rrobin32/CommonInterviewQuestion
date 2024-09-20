c96887b84d531049023949ed51142d2e45555912
rakesh <rakeshrobin32153@gmail.com>
2024-09-20 22:10:28 +0530

    Find Second Largest Element in array

--- CommonInterviewQuestion/SecondLargestElement.cs
+++ CommonInterviewQuestion/SecondLargestElement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CommonInterviewQuestion
 {
@@ -10,9 +11,9 @@ namespace CommonInterviewQuestion
     {
         public static void Main(string[] args)
         {
-            int[] arr = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
+            int[] array = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
             int largestElement = int.MinValue, secondLargestElement = int.MinValue;
-            foreach (int i in arr)
+            foreach (int i in array)
             {
                 if (i > largestElement)
                 {
