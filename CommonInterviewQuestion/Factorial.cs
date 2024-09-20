5ea4c73f6fbe4727904c78b21bfa809b910458eb
rakesh <rakeshrobin32153@gmail.com>
2024-09-20 22:10:04 +0530

    Factorial

---
+++ CommonInterviewQuestion/Factorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterviewQuestion
+{
+    internal class Factorial
+    {
+        public static void Main(string[] args)
+        {
+            int input = 4;
+            int result = findFactorial(input);
+
+            Console.WriteLine(result);  
+        }
+
+        public static int findFactorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * findFactorial(number - 1);
+        }
+    }
+}
